1bc90fbb1d1c1d879b29f55a52f5c559a7cec704
Pratik Akhare <akharepratik9@gmail.com>
2026-04-29 19:04:00 +0530

    Update ALLOWED_HOSTS for deployment

--- rfqsite/settings.py
+++ rfqsite/settings.py
@@ -5,7 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-replace-this-in-production'
 DEBUG = True
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [ "rfq-99w5.onrender.com",
+    "localhost",
+    "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
